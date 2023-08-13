30132d7858435fed090ea2aeb0bd68329c8fb954
vibhoraggarwal <vibhorag24@gmail.com>
2023-08-13 11:47:32 +0200

    testing load initialization on videocapture

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <main.h>
-using namespace std;
+#include <opencv2/videoio.hpp>
 
+using namespace std;
+using namespace cv;
 int main()
 {
-    cout<< "Hello World";
+    cout<< "Hello World \n";
+    VideoCapture cap{VideoCapture(0)};
+    int num{3};
+    cout << num << endl;
     return 0;
 }
