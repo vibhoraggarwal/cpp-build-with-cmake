b19c356c288198a16e2260d5f33621b1675e5799
vibhor <agv2fe@bosch.com>
2025-01-08 10:38:49 +0100

    adding replace string

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
 #include <main.h>
-#include <opencv2/videoio.hpp>
 
 using namespace std;
-using namespace cv;
 int main()
 {
     cout<< "Hello World \n";
-    VideoCapture cap{VideoCapture(0)};
     int num{3};
     cout << num << endl;
     return 0;
